ec5fe604f46c9340f148f0be6cfb60a00fb885cd
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-21 19:37:32 +0330

    Create plans.py

---
+++ plans.py
@@ -0,0 +1,24 @@
+PLANS = {
+
+    "plan1": {
+        "name": "🥉 یک ماهه",
+        "days": 30,
+        "volume": 100,
+        "price": 150000
+    },
+
+    "plan2": {
+        "name": "🥈 سه ماهه",
+        "days": 90,
+        "volume": 300,
+        "price": 390000
+    },
+
+    "plan3": {
+        "name": "🥇 شش ماهه",
+        "days": 180,
+        "volume": 700,
+        "price": 690000
+    }
+
+}
